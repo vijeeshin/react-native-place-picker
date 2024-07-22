89ced3f59e7a338f1ffe6ec39ce9c113b054b1a1
Vigeesh Kumar V G <mr.vijeeshkumar@gmail.com>
2024-07-22 17:28:11 +0530

    Update PlacePickerActivity.kt

--- android/src/main/java/com/placepicker/PlacePickerActivity.kt
+++ android/src/main/java/com/placepicker/PlacePickerActivity.kt
@@ -96,7 +96,7 @@ class PlacePickerActivity : AppCompatActivity(), OnMapReadyCallback,
       if (task == null) {
         Toast.makeText(this, "Unable to get location", Toast.LENGTH_SHORT).show()
       } else {
-        if(options.initialCoordinates.latitude != 0 && options.initialCoordinates.longitude != 0) {
+        if(options.initialCoordinates.latitude != 0.toDouble() && options.initialCoordinates.longitude != 0.toDouble()) {
           mMap.moveCamera(
             CameraUpdateFactory.newLatLngZoom(
               LatLng(
